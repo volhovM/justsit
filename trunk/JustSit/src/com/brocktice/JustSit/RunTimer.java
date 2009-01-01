81b20700673eeabecf8b4c2e6156d414599107e2
brock.tice <brock.tice@1e3857fe-d6ca-11dd-918e-25ffe7a15323>
2009-01-01 21:48:19 +0000

    Added copyright notices and GPL. Added preferences for airplane mode, silent ringer mode, and keeping the screen on. Added an about screen. Added checking of input values and warnings to the user if invalid values are used.

--- trunk/JustSit/src/com/brocktice/JustSit/RunTimer.java
+++ trunk/JustSit/src/com/brocktice/JustSit/RunTimer.java
@@ -1,6 +1,26 @@
+// Copyright 2008 Brock M. Tice
+/*  This file is part of JustSit.
+
+    JustSit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JustSit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JustSit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.brocktice.JustSit;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.TextView;
@@ -8,11 +28,14 @@ import android.widget.TextView;
 public class RunTimer extends Activity {
 	private TextView mTimerView;
 	private TextView mTimerLabel;
+
+	
 	public static final String PREFS_NAME = "JustSitPreferences";
 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.run_timer);
 		mTimerView = (TextView) findViewById(R.id.timer_view);
@@ -26,8 +49,7 @@ public class RunTimer extends Activity {
 			runCountdown(timer_duration);
 		}
 	}
-	
-	
+
 	protected void runCountdown(int start_time){
 		new CountDownTimer(start_time, 1000) {
 
